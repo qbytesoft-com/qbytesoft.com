62c6d15115a5d18f93d95d3dbfa631e6cf4ee932
MohammadShawon <13203016@iubat.edu>
2020-03-27 20:25:52 +0600

    domain name remove

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,12 @@
 <?php
-Route::group(function (){
-    Route::namespace('Website')->group(function (){
-       Route::get('/','PageController@index')->name('qbytesoft.home');
-       Route::get('/web-development','PageController@webDevelopment')->name('qbytesoft.web-development');
-       Route::get('/open-source','PageController@openSource')->name('qbytesoft.open-source');
-       Route::get('/laravel','PageController@laravel')->name('qbytesoft.laravel');
-       Route::get('/about','PageController@about')->name('qbytesoft.about');
-    });
+
+use Illuminate\Support\Facades\Route;
+
+Route::namespace('Website')->group(function (){
+    Route::get('/','PageController@index')->name('qbytesoft.home');
+    Route::get('/web-development','PageController@webDevelopment')->name('qbytesoft.web-development');
+    Route::get('/open-source','PageController@openSource')->name('qbytesoft.open-source');
+    Route::get('/laravel','PageController@laravel')->name('qbytesoft.laravel');
+    Route::get('/about','PageController@about')->name('qbytesoft.about');
 });
 
