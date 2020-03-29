722205ce27a9edef419243e8160e7853a68bdb67
MohammadShawon <13203016@iubat.edu>
2020-03-29 12:45:26 +0600

    cache package trying

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -35,6 +35,7 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \Silber\PageCache\Middleware\CacheResponse::class,
         ],
 
         'api' => [
@@ -61,6 +62,7 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'page-cache' => \Silber\PageCache\Middleware\CacheResponse::class,
     ];
 
     /**
